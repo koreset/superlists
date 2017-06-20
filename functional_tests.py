e5352813987653f82787c22383685bf26d916159
Jome Akpoduado <jome@Jomes-iMac.yookos.local>
2017-06-20 13:47:37 +0200

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,14 +18,24 @@ class NewVisitorTest(unittest.TestCase):
 
 		# He notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+		
 
 		# He is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# He enters "Learn TDD the right way" into a text box (Jome's hobby is writing code)
-
-		# When he hits Enter, the page updated, and noe the page lists
+		inputbox.send_keys('Learn TDD the right way')
+		# When he hits Enter, the page updated, and now the page lists
 		# "1: Learn TDD the right way" as an item in the to-do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text) == '1: Learn TDD the right way' for row in rows)
 
 		# There is still a text box inviting her to add another item. He enters
 		# "Setup iMac to complete the course"
@@ -37,6 +48,7 @@ class NewVisitorTest(unittest.TestCase):
 		# He visits the URL - hsi to-do list is still there.
 
 		# He goes to play FIFA
+		self.fail('Finish the test!')
 
 
 if __name__ == "__main__":
