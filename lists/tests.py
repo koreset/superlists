0b35fa22780df205e1051dd0677b86aa30a7ac7a
Jome Akpoduado <jomea@yookos.com>
2017-06-21 08:55:19 +0200

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 import re
+from lists.models import Item
 
 
 # Create your tests here.
@@ -33,3 +34,22 @@ class HomePageTest(TestCase):
 		self.assertIn('A new list item', self.remove_csrf(response.content.decode()))
 		expected_html = render_to_string('home.html', {'new_item_text' : 'A new list item'})
 		self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = "The first todo item"
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = "The second todo item"
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, "The first todo item")
+		self.assertEqual(second_saved_item.text, "The second todo item")
+
--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	print (request.POST)
-
 	return render(request, 'home.html', {'new_item_text':request.POST.get('item_text', '')})
 	
