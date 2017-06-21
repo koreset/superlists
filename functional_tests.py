87d4bd6ea3fe038e4e41c94c44647e88cfd1cf48
Jome Akpoduado <jomea@yookos.com>
2017-06-21 08:16:35 +0200

    made changes to functional tests, added post methods to views Please enter the commit message for your changes. Lines starting

--- functional_tests.py
+++ functional_tests.py
@@ -36,11 +36,22 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Learn TDD the right way' for row in rows), 
-			 "New to-do item did not appear in the table")
+
+		self.assertIn('1: Learn TDD the right way', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item. He enters
 		# "Setup iMac to complete the course"
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Setup iMac to complete the course')
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+
+		self.assertIn('1: Learn TDD the right way', [row.text for row in rows])
+		self.assertIn('2: Setup iMac to complete the course', [row.text for row in rows])
+
+
 
 		# The page updates again and shows both items on his list
 
